61b773ebbaca77b2b8cdc9757f5dedf666a6b69c
Manuel Zavala <majezanu@gmail.com>
2020-02-27 23:03:17 -0600

    feat: add restaurants data

---
+++ src/model/data/restaurants.js
@@ -0,0 +1,19 @@
+let restaurants = [
+    {
+        id: 1,
+        name: 'BBT',
+        zone: 'centro'
+    },
+    {
+        id: 2,
+        name: 'La cheleria',
+        zone: 'sur'
+    },
+    {
+        id: 3,
+        name: 'Vips',
+        zone: 'centro'
+    }
+];
+
+export default restaurants;
