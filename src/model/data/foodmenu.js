fb32d0edfb92b957a49fcc23762aaff70c769892
Manuel Zavala <majezanu@gmail.com>
2020-03-07 14:53:12 -0600

    fix: change id to uuid

--- src/model/data/foodmenu.js
+++ src/model/data/foodmenu.js
@@ -1,48 +1,48 @@
 let foodmenu = [
     {
-        id: 1,
+        id: '172e9478-e39a-4246-a157-2a50fad7817b',
         name: 'The Full Monty',
         restaurant_id: 'eb6f95a1-2df4-401a-a568-6a2859474e06',
         price: 299
     },
     {
-        id: 2,
+        id: '2642e81d-1b29-4f9b-9f45-68a552a0d3f9',
         name: 'Buffalo o BBQ Wings & Fries',
         restaurant_id: 'eb6f95a1-2df4-401a-a568-6a2859474e06',
         price: 129
     },
     {
-        id: 3,
+        id: '885d6c33-b876-40c7-af06-09201605a671',
         name: 'Mozzarella Cheese Sticks',
         restaurant_id: 'eb6f95a1-2df4-401a-a568-6a2859474e06',
         price: 149
     },
     {
-        id: 4,
+        id: 'a255c95d-c02e-42eb-9ef8-c34b7fd16941',
         name: 'Wings',
         restaurant_id: 'ade5c625-dc44-4ce4-9cf5-49c7125fa982',
         price: 100
     },
     {
-        id: 5,
+        id: '2a455331-39ca-49ea-ae6a-ff7f1ee2eb7c',
         name: 'Pasta Alfredo',
         restaurant_id: 'ade5c625-dc44-4ce4-9cf5-49c7125fa982',
         price: 150
     },
     {
-        id: 6,
+        id: '90a84b75-8ee9-4e7f-a9a0-310082d5514f',
         name: 'Tacos dorados de pollo',
         restaurant_id: 'e84eaa78-d365-41cc-b80b-0ec50c0febe2',
         price: 138
     },
     {
-        id: 7,
+        id: '5e29e525-2469-4044-b4d2-46ba69f967b7',
         name: 'Enchiladas de mole',
         restaurant_id: 'e84eaa78-d365-41cc-b80b-0ec50c0febe2',
         price: 150
     },
     {
-        id: 5,
+        id: '7a70889a-99a3-45ac-b697-1d9f1031fde4',
         name: 'Tacos de Arrachera Harina',
         restaurant_id: 'e84eaa78-d365-41cc-b80b-0ec50c0febe2',
         price: 212
